047d4b85c24ca5c3b1f5b9c0c88696ba95aa80e7
AndreiBereta <andreibereta@gmail.com>
2024-01-23 11:46:08 -0300

    connection

--- Parte_2/Servidor/connections.hpp
+++ Parte_2/Servidor/connections.hpp
@@ -2,19 +2,11 @@
 #include <vector>
 #include <netinet/in.h>
 #include <string>
-
-#define DIMENSAO_GERAL 100
-
-//TODO
-typedef struct {
-    alignas(ALIGN_VALUE) char numero_porta[DIMENSAO_GERAL]; // buffer
-    alignas(ALIGN_VALUE) char endereco_ip[DIMENSAO_GERAL]; // &buffer[104]
-    alignas(ALIGN_VALUE) char username[DIMENSAO_GERAL]; // &buffer[104 * 2]
-    alignas(ALIGN_VALUE) int socket_id; // &buffer[104 * 3]
-} Connection_t;
+#include "../Common/package.hpp"
 
 // struct que guarda todas as conexões ativas do servidor principal
-typedef struct {
+typedef struct
+{
     // Lock para alterar as conexões
     pthread_mutex_t *lock; // NOVO MUTEX
 
